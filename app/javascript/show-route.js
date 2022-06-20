9e3a1b98367919e6b25e5ded1f69297f02d9fac3
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-06-20 18:22:52 +0900

    Change: 翻訳の追加、レイアウトの修正、往復の距離を入力、menuのボタンの当たり判定を修正

--- app/javascript/show-route.js
+++ app/javascript/show-route.js
@@ -26,8 +26,8 @@ function initMap() {
       result['routes'][0]['legs'][0]['end_address'] = destination['name']
       directionsRenderer.setDirections(result);
       distance = result['routes'][0]['legs'][0]['distance']['value']
-      document.getElementById('js-get-distance').value = distance
-      // document.getElementById('js-create-distance-link').setAttribute('href', `/destinations/not_save?distance=${distance}`);
+      document.getElementById('js-show-distance').textContent = `片道: ${distance}m`
+      document.getElementById('js-get-distance').value = distance * 2
     }
   });
 }
