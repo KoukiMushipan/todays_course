16a104aaa0800ea11de2863997690748df6f7248
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-07-01 14:50:13 +0900

    Improve: historyの翻訳

--- app/decorators/history_decorator.rb
+++ app/decorators/history_decorator.rb
@@ -6,4 +6,11 @@ class HistoryDecorator < ApplicationDecorator
     time.ceil.to_s
   end
 
+  def from_what_time_to_what_time
+    l(history.start_time, format: :short) + I18n.t('defaults.range_symbol') + l(history.end_time, format: :very_short)
+  end
+
+  def from_where_to_where
+    departure.location.name + I18n.t('defaults.range_symbol') + destination.location.name
+  end
 end
