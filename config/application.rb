940564870ce8dd3317cab7c62c5c7e413b0c9d49
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-09-04 15:10:03 +0900

    Add: 設定の追加とModelの翻訳追加 generate、翻訳、timezoneの設定 modelのみ簡単に翻訳を作成

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,18 @@ module TodaysCourse
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = %i[ja]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.helper false
+    end
   end
 end
