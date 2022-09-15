4ca0cb9cd43bfc6feaf505953565e3e7ca65634f
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-09-15 17:07:35 +0900

    Add: profile周りを仮で作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,30 @@
 class ProfilesController < ApplicationController
-  def show
-  end
+  before_action :set_user, only: %i[show edit update destroy]
 
-  def edit
-  end
+  def show; end
+
+  def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to profile_path, notice: 'ユーザー情報を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @user.destroy!
+    redirect_to login_path, status: :see_other, notice: 'ユーザーを削除しました'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
