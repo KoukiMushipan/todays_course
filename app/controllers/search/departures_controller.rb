a6038e18f56dbcc6f3a1f36e38e79e5b5bf516a5
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-09-26 15:13:58 +0900

    Add: 保存済みのレイアウト作成

--- app/controllers/search/departures_controller.rb
+++ app/controllers/search/departures_controller.rb
@@ -1,7 +1,7 @@
 class Search::DeparturesController < ApplicationController
   def new
-    @departures = current_user.departures.where(is_saved: true)
-    @histories = current_user.histories.includes(destination: :departure)
+    @departures = current_user.departures.where(is_saved: true).includes(:location).order(created_at: :desc)
+    @histories = current_user.histories.includes(destination: [departure: :location], destination: :location).order(created_at: :desc)
   end
 
   def create
