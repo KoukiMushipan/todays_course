2c487777fb98393ea57db48b3a08eaf6ef058ce2
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-09-28 07:14:19 +0900

    Add: swiperの導入

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+import Swiper, { Navigation, Pagination, Keyboard, EffectCreative } from 'swiper'
+Swiper.use([Navigation, Pagination, Keyboard, EffectCreative])
+
+export default class extends Controller {
+  connect() {
+    const swiperList = document.getElementsByClassName('swiper')
+    for (let i = 1; i <= swiperList.length; i++) {
+      new Swiper(`.swiper-container-${i}`, {
+        loop: true,
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: ["-120%", 0, -500],
+          },
+          next: {
+            shadow: true,
+            translate: ["120%", 0, -500],
+          },
+        },
+        keyboard: {
+          enabled: true,
+        },
+        pagination: {
+          el: `.swiper-pagination-${i}`,
+          clickable: true,
+        },
+        navigation: {
+          nextEl: `.swiper-button-next-${i}`,
+          prevEl: `.swiper-button-prev-${i}`,
+        },
+      });
+    }
+  }
+}
