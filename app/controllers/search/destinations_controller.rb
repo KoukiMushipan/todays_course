afee8f0a7944d75443f117651f342926d846f040
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-11-07 21:21:00 +0900

    Fix: SearchDestinationFormをSearchTermFormに変更

--- app/controllers/search/destinations_controller.rb
+++ app/controllers/search/destinations_controller.rb
@@ -13,38 +13,38 @@ class Search::DestinationsController < ApplicationController
     departure = Departure.find_by(uuid: params[:departure])
     if departure
       @departure_info, session[:departure] = Array.new(2, departure.attributes_for_session)
-      @search_destination = SearchDestinationForm.new
+      @search_term = SearchTermForm.new
     elsif session[:departure]
       @departure_info = session[:departure]
-      @search_destination = SearchDestinationForm.new
+      @search_term = SearchTermForm.new
     else
       redirect_to new_search_departure_path, flash: {error: "出発地が設定されていません"}
     end
   end
 
   def create
-    @search_destination = SearchDestinationForm.new(search_destination_params)
+    @search_term = SearchTermForm.new(search_term_params)
 
-    if !@search_destination.valid?
+    if !@search_term.valid?
       @departure_info = session[:departure]
       flash.now[:error] = '入力情報に誤りがあります'
       return render :new, status: :unprocessable_entity
     end
 
-    results = RequestNearbyService.new(session[:departure], @search_destination.radius, @search_destination.type).call
+    results = RequestNearbyService.new(session[:departure], @search_term.radius, @search_term.type).call
     if !results
       @departure_info = session[:departure]
       flash.now[:error] = '目的地が見つかりませんでした'
       return render :new, status: :unprocessable_entity
     end
 
-    session[:results], session[:destination_terms] = results, @search_destination.attributes
+    session[:results], session[:destination_terms] = results, @search_term.attributes
     redirect_to search_destinations_path
   end
 
   private
 
-  def search_destination_params
-    params.require(:search_destination_form).permit(:radius, :type)
+  def search_term_params
+    params.require(:search_term_form).permit(:radius, :type)
   end
 end
