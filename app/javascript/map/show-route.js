45418c33144f797f5891c82abb6b338cc5953522
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-11-12 15:21:33 +0900

    Add: histories#newに直にアクセスした場合でも、適切に表示されるよう追加

--- app/javascript/map/show-route.js
+++ app/javascript/map/show-route.js
@@ -1,6 +1,16 @@
 window.showRoute = () => {
   const departure = gon.locationInfo.departure;
   const destination = gon.locationInfo.destination;
+  let destinationName
+  let destinationAddress
+
+  if (destination.fixed && destination.variable) {
+    destinationName = destination.variable.name;
+    destinationAddress = destination.fixed.address;
+  } else {
+    destinationName = destination.name;
+    destinationAddress = destination.address;
+  }
 
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
@@ -16,21 +26,23 @@ window.showRoute = () => {
 
   const request = {
     origin: departure.address,
-    destination: destination.fixed.address,
+    destination: destinationAddress,
     travelMode: 'WALKING'
   };
 
   directionsService.route(request, function (result, status) {
     if (status == 'OK') {
       result['routes'][0]['legs'][0]['start_address'] = departure.name;
-      result['routes'][0]['legs'][0]['end_address'] = destination.variable.name;
+      result['routes'][0]['legs'][0]['end_address'] = destinationName;
 
       directionsRenderer.setDirections(result);
 
-      const distance = result['routes'][0]['legs'][0]['distance']['value'];
-      document.getElementById('js-show-distance').textContent = `片道: ${distance}m`;
-      document.getElementById('destination_form_name').value = destination.variable.name;
-      document.getElementById('destination_form_distance').value = distance;
+      if (document.getElementById('js-input-area')) {
+        const distance = result['routes'][0]['legs'][0]['distance']['value'];
+        document.getElementById('js-show-distance').textContent = `片道: ${distance}m`;
+        document.getElementById('destination_form_name').value = destinationName;
+        document.getElementById('destination_form_distance').value = distance;
+      }
     }
   });
 }
