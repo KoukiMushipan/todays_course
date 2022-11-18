bda36e7f70d872456eb340971672c59829c94075
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-11-18 14:04:45 +0900

    Change: start_timeはhistory作成時に自動で行われるよう変更

--- app/models/history.rb
+++ app/models/history.rb
@@ -3,10 +3,10 @@ class History < ApplicationRecord
   belongs_to :destination
   has_one :departure, through: :destination
 
-  validates :start_time, presence: true
   validates :moving_distance, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 42195}
 
   before_create -> { self.uuid = SecureRandom.uuid }
+  before_create -> { self.start_time = DateTime.now }
 
   scope :with_location, -> { includes(destination: [departure: :location], destination: :location) }
   scope :recent, -> { order(created_at: :desc) }
