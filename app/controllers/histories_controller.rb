1ab54e09319d91f4d1935c89d1f1fef68cd8d32b
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-11-21 22:40:05 +0900

    Add: history#update, destroyを作成

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,6 +2,7 @@ class HistoriesController < ApplicationController
   before_action :check_course_params, only: %i[create]
   before_action :check_departure_session_and_set_departure_info, only: %i[create show]
   before_action :check_destination_session_and_set_destination_info, only: %i[create show]
+  before_action :set_history, only: %i[show update destroy]
 
   def new # destinations#newから遷移の場合、turbo_frameリクエスト
     destination = current_user.destinations.find_by(uuid: params[:destination])
@@ -22,7 +23,32 @@ class HistoriesController < ApplicationController
   end
 
   def show
-    @history = current_user.histories.find_by(uuid: params[:id])
     gon.locationInfo = {departure: @departure_info, destination: @destination_info}
+  end
+
+  def update
+    if @history.end_time
+
+    else
+      @history.update!(end_time: DateTime.now)
+      redirect_to history_path(@history.uuid), status: :see_other
+    end
+  end
+
+  def destroy
+    if @history.end_time
+      @history.destroy!
+      redirect_to profile_path, status: :see_other
+    else
+      destination = @history.destination
+      @history.destroy!
+      redirect_to new_history_path(destination: destination.uuid), status: :see_other
+    end
+  end
+
+  private
+
+  def set_history
+    @history = current_user.histories.find_by!(uuid: params[:id])
   end
 end
