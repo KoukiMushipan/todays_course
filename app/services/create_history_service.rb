c11d8812ddbb7c5a81162e4c5c6e732fff2009f5
KoukiMushipan <mushipan.gyunyu@gmail.com>
2022-11-23 12:12:10 +0900

    Add: CreateHistoryServiceのリファクタリング

--- app/services/create_history_service.rb
+++ app/services/create_history_service.rb
@@ -4,20 +4,38 @@ class CreateHistoryService
   end
 
   def call
+    destination = prepare_location_flexibly
+    create_history(destination)
+  end
+
+  private
+
+  attr_reader :user, :departure_info, :destination_info, :course_type
+
+  def create_departure
+    location = Location.create!(departure_info)
+    departure = user.departures.create!(location: location)
+  end
+
+  def create_destination
+    location = Location.create!(destination_info.select { |k, v| k != :distance })
+    user.destinations.create!(location: location, departure: departure, distance: destination_info[:distance])
+  end
+
+  def prepare_location_flexibly
     if destination_info[:uuid]
       destination = user.destinations.find_by!(uuid: destination_info[:uuid])
-      departure = destination.departure
+      destination.departure
     elsif departure_info[:uuid]
       departure = user.departures.find_by!(uuid: departure_info[:uuid])
-      location = Location.create!(destination_info.select { |k, v| k != :distance })
-      destination = user.destinations.create!(location: location, departure: departure, distance: destination_info[:distance])
+      create_destination
     else
-      departure_location = Location.create!(departure_info)
-      departure = user.departures.create!(location: departure_location)
-      destination_location = Location.create!(destination_info.select { |k, v| k != :distance })
-      destination = user.destinations.create!(location: destination_location, departure: departure, distance: destination_info[:distance])
+      departure = create_departure
+      create_destination
     end
+  end
 
+  def create_history(destination)
     case course_type
     when 'one_way'
       history = user.histories.create!(destination: destination, moving_distance: destination.distance)
@@ -27,8 +45,4 @@ class CreateHistoryService
       {history: history, success: 'スタートしました(往復)'}
     end
   end
-
-  private
-
-  attr_reader :user, :departure_info, :destination_info, :course_type
 end
