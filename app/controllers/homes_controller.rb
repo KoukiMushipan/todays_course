6e5eab08dd4c8980e79b9369f159f1eddd81d1ab
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-01-02 14:38:38 +0900

    Add: 利用規約・プライバシーポリシー・お問い合わせ追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,10 @@
 class HomesController < ApplicationController
-  layout 'top', only: %i[top]
+  layout 'top', only: %i[top terms]
 
   skip_before_action :require_login
   skip_before_action :check_not_finished
 
   def top; end
+
+  def terms; end
 end
