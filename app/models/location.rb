2d1c3558f67eb4409475fede127443db6ffeca1b
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-14 13:33:12 +0900

    Add: Locationのvalidationと翻訳

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,7 +5,8 @@ class Location < ApplicationRecord
   accepts_nested_attributes_for :destination
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  validates :latitude, presence: true, numericality: { in: -90..90 }
+  validates :longitude, presence: true, numericality: { in: -180..180 }
   validates :address, presence: true, length: { maximum: 255 }
+  validates :place_id, presence: true, length: { maximum: 255 }
 end
