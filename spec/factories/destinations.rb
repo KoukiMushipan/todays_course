f22783dde1d58736db20217cea74ec16ead85e6f
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-20 22:58:43 +0900

    Fix: FactoryBotでuuid生成する必要ないので削除

--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :destination do
-    uuid { SecureRandom.uuid }
     distance { 1000 }
     association :user
     association :location
--- spec/factories/histories.rb
+++ spec/factories/histories.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :history do
-    uuid { SecureRandom.uuid }
     moving_distance { 2000 }
     start_time { Time.zone.now }
     end_time { Time.zone.now.since(1.hour) }
