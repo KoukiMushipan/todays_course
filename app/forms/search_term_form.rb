a03c92d5bef6881eb5e5f209c0dd71ec08a07eaf
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-20 23:54:35 +0900

    Add: SearchTermForm, GuestFormに念の為validation #name, #type

--- app/forms/search_term_form.rb
+++ app/forms/search_term_form.rb
@@ -6,5 +6,5 @@ class SearchTermForm
   attribute :type, :string
 
   validates :radius, presence: true, numericality: { only_integer: true, in: 1_000..5_000 }
-  validates :type, presence: true, inclusion: { in: Settings.google.place_type.to_h.values }
+  validates :type, presence: true, inclusion: { in: Settings.google.place_type.to_h.values }, length: { maximum: 255 }
 end
