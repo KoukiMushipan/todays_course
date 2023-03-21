12b432842c0260d6e3ccdf5984b05691ef897e31
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-21 15:13:24 +0900

    Fix: describe・contextの階層を追加・修正

--- spec/system/settings_spec.rb
+++ spec/system/settings_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe "Settings", type: :system do
   describe 'ユーザー設定' do
     before { login(user) }
 
-    it '正常にログアウトする' do
-      find('label[for=right]').click
-      page.accept_confirm('ログアウトしますか？') do
-        click_link 'ログアウト'
+    context 'ログアウトする' do
+      it '正常にログアウトし、ログインページに戻る' do
+        find('label[for=right]').click
+        page.accept_confirm('ログアウトしますか？') do
+          click_link 'ログアウト'
+        end
+        expect(page).to have_content 'ログアウトしました'
+        expect(current_path).to eq login_path
       end
-      expect(page).to have_content 'ログアウトしました'
-      expect(current_path).to eq login_path
     end
 
     describe 'ユーザーのプロフィールを編集' do
@@ -20,68 +22,70 @@ RSpec.describe "Settings", type: :system do
         click_link '編集'
       end
 
-      context '正常な値を入力する' do
-        it 'プロフィールの更新が成功し、プロフィールページに戻る' do
-          fill_in '名前', with: 'user-name-update'
-          fill_in 'メールアドレス', with: 'user-email-update@example.com'
-          click_button '更新'
-          expect(page).to have_content '編集'
-          expect(current_path).to eq profile_path
-        end
-      end
-
-      describe 'Validations#name' do
-        context '名前を入力しない' do
+      describe 'Validations' do
+        context '正常な値を入力する' do
           it 'プロフィールの更新が成功し、プロフィールページに戻る' do
-          fill_in '名前', with: ''
-          click_button '更新'
-          expect(page).to have_content '編集'
-          expect(current_path).to eq profile_path
-          end
-        end
-
-        context '名前を50文字入力する' do
-          it 'プロフィールの更新が成功し、プロフィールページに戻る' do
-          fill_in '名前', with: 'a' * 50
-          click_button '更新'
-          expect(page).to have_content '編集'
-          expect(current_path).to eq profile_path
-          end
-        end
-
-        context '名前を51文字入力する' do
-          it 'プロフィールの更新が失敗し、フォームが表示されたままになる' do
-            fill_in '名前', with: 'a' * 51
+            fill_in '名前', with: 'user-name-update'
+            fill_in 'メールアドレス', with: 'user-email-update@example.com'
             click_button '更新'
-            expect(page).to have_content '入力情報に誤りがあります'
-            expect(page).to have_content '名前は50文字以内で入力してください'
-            expect(page).to have_button '更新'
-            expect(current_path).to eq profile_path
-          end
-        end
-      end
-
-      describe 'Validations#email' do
-        context 'メールアドレスを入力しない' do
-          it 'プロフィールの更新が失敗し、フォームが表示されたままになる' do
-            fill_in 'メールアドレス', with: ''
-            click_button '更新'
-            expect(page).to have_content '入力情報に誤りがあります'
-            expect(page).to have_content 'メールアドレスを入力してください'
-            expect(page).to have_button '更新'
+            expect(page).to have_content '編集'
             expect(current_path).to eq profile_path
           end
         end
 
-        context '重複したメールアドレスを入力する' do
-          it 'プロフィールの更新が失敗し、フォームが表示されたままになる' do
-            duplicate_user = create(:user)
-            fill_in 'メールアドレス', with: duplicate_user.email
+        describe '#name' do
+          context '名前を入力しない' do
+            it 'プロフィールの更新が成功し、プロフィールページに戻る' do
+            fill_in '名前', with: ''
             click_button '更新'
-            expect(page).to have_content '入力情報に誤りがあります'
-            expect(page).to have_content 'メールアドレスはすでに存在します'
-            expect(page).to have_button '更新'
+            expect(page).to have_content '編集'
             expect(current_path).to eq profile_path
+            end
+          end
+
+          context '名前を50文字入力する' do
+            it 'プロフィールの更新が成功し、プロフィールページに戻る' do
+            fill_in '名前', with: 'a' * 50
+            click_button '更新'
+            expect(page).to have_content '編集'
+            expect(current_path).to eq profile_path
+            end
+          end
+
+          context '名前を51文字入力する' do
+            it 'プロフィールの更新が失敗し、フォームが表示されたままになる' do
+              fill_in '名前', with: 'a' * 51
+              click_button '更新'
+              expect(page).to have_content '入力情報に誤りがあります'
+              expect(page).to have_content '名前は50文字以内で入力してください'
+              expect(page).to have_button '更新'
+              expect(current_path).to eq profile_path
+            end
+          end
+        end
+
+        describe '#email' do
+          context 'メールアドレスを入力しない' do
+            it 'プロフィールの更新が失敗し、フォームが表示されたままになる' do
+              fill_in 'メールアドレス', with: ''
+              click_button '更新'
+              expect(page).to have_content '入力情報に誤りがあります'
+              expect(page).to have_content 'メールアドレスを入力してください'
+              expect(page).to have_button '更新'
+              expect(current_path).to eq profile_path
+            end
+          end
+
+          context '重複したメールアドレスを入力する' do
+            it 'プロフィールの更新が失敗し、フォームが表示されたままになる' do
+              duplicate_user = create(:user)
+              fill_in 'メールアドレス', with: duplicate_user.email
+              click_button '更新'
+              expect(page).to have_content '入力情報に誤りがあります'
+              expect(page).to have_content 'メールアドレスはすでに存在します'
+              expect(page).to have_button '更新'
+              expect(current_path).to eq profile_path
+            end
           end
         end
       end
--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -5,125 +5,127 @@ RSpec.describe 'Signup', type: :system do
   describe 'ユーザー新規登録' do
     before { visit signup_path }
 
-    context '正常な値を入力する' do
-      it 'ユーザーの新規作成が成功し、プロフィールページに遷移する' do
-        fill_in '名前', with: 'user-name'
-        fill_in 'メールアドレス', with: 'user-email@example.com'
-        fill_in 'パスワード', with: 'user-password'
-        fill_in 'パスワード（確認用）', with: 'user-password'
-        click_button '登録'
-        expect(page).to have_content '新規作成に成功しました'
-        expect(current_path).to eq profile_path
-      end
-    end
-
-    describe 'Validations#name' do
-      before do
-        fill_in 'メールアドレス', with: 'user-email@example.com'
-        fill_in 'パスワード', with: 'user-password'
-        fill_in 'パスワード（確認用）', with: 'user-password'
-      end
-
-      context '名前を入力しない' do
+    describe 'Validations' do
+      context '正常な値を入力する' do
         it 'ユーザーの新規作成が成功し、プロフィールページに遷移する' do
-          click_button '登録'
-          expect(page).to have_content '新規作成に成功しました'
-          expect(current_path).to eq profile_path
-        end
-      end
-
-      context '名前を50文字入力する' do
-        it 'ユーザーの新規作成が成功し、プロフィールページに遷移する' do
-          fill_in '名前', with: 'a' * 50
-          click_button '登録'
-          expect(page).to have_content '新規作成に成功しました'
-          expect(current_path).to eq profile_path
-        end
-      end
-
-      context '名前を51文字入力する' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
-          fill_in '名前', with: 'a' * 51
-          click_button '登録'
-          expect(page).to have_content '名前は50文字以内で入力してください'
-          expect(page).to have_content '新規作成に失敗しました'
-          expect(current_path).to eq signup_path
-        end
-      end
-    end
-
-    describe 'Validations#email' do
-      before do
-        fill_in '名前', with: 'user-name'
-        fill_in 'パスワード', with: 'user-password'
-        fill_in 'パスワード（確認用）', with: 'user-password'
-      end
-
-      context 'メールアドレスを入力しない' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
-          click_button '登録'
-          expect(page).to have_content 'メールアドレスを入力してください'
-          expect(page).to have_content '新規作成に失敗しました'
-          expect(current_path).to eq signup_path
-        end
-      end
-
-      context '重複したメールアドレスを入力する' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
-          fill_in 'メールアドレス', with: user.email
-          click_button '登録'
-          expect(page).to have_content 'メールアドレスはすでに存在します'
-          expect(page).to have_content '新規作成に失敗しました'
-          expect(current_path).to eq signup_path
-        end
-      end
-    end
-
-    describe 'Validations#password, password_confirmation' do
-      before do
-        fill_in '名前', with: 'user-name'
-        fill_in 'メールアドレス', with: 'user-email@example.com'
-      end
-
-      context 'パスワードを入力しない' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+          fill_in '名前', with: 'user-name'
+          fill_in 'メールアドレス', with: 'user-email@example.com'
+          fill_in 'パスワード', with: 'user-password'
           fill_in 'パスワード（確認用）', with: 'user-password'
           click_button '登録'
-          expect(page).to have_content 'パスワードは3文字以上で入力してください'
-          expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
-          expect(page).to have_content '新規作成に失敗しました'
-          expect(current_path).to eq signup_path
-        end
-      end
-
-      context 'パスワード（確認用）を入力しない' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
-          fill_in 'パスワード', with: 'user-password'
-          click_button '登録'
-          expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
-          expect(page).to have_content '新規作成に失敗しました'
-          expect(current_path).to eq signup_path
-        end
-      end
-
-      context 'パスワードを2文字入力する' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
-          fill_in 'パスワード', with: 'a' * 2
-          fill_in 'パスワード（確認用）', with: 'a' * 2
-          click_button '登録'
-          expect(page).to have_content 'パスワードは3文字以上で入力してください'
-          expect(page).to have_content '新規作成に失敗しました'
-          expect(current_path).to eq signup_path
-        end
-      end
-
-      context 'パスワードを3文字入力する' do
-        it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
-          fill_in 'パスワード', with: 'a' * 3
-          fill_in 'パスワード（確認用）', with: 'a' * 3
-          click_button '登録'
           expect(page).to have_content '新規作成に成功しました'
           expect(current_path).to eq profile_path
+        end
+      end
+
+      describe '#name' do
+        before do
+          fill_in 'メールアドレス', with: 'user-email@example.com'
+          fill_in 'パスワード', with: 'user-password'
+          fill_in 'パスワード（確認用）', with: 'user-password'
+        end
+
+        context '名前を入力しない' do
+          it 'ユーザーの新規作成が成功し、プロフィールページに遷移する' do
+            click_button '登録'
+            expect(page).to have_content '新規作成に成功しました'
+            expect(current_path).to eq profile_path
+          end
+        end
+
+        context '名前を50文字入力する' do
+          it 'ユーザーの新規作成が成功し、プロフィールページに遷移する' do
+            fill_in '名前', with: 'a' * 50
+            click_button '登録'
+            expect(page).to have_content '新規作成に成功しました'
+            expect(current_path).to eq profile_path
+          end
+        end
+
+        context '名前を51文字入力する' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            fill_in '名前', with: 'a' * 51
+            click_button '登録'
+            expect(page).to have_content '名前は50文字以内で入力してください'
+            expect(page).to have_content '新規作成に失敗しました'
+            expect(current_path).to eq signup_path
+          end
+        end
+      end
+
+      describe '#email' do
+        before do
+          fill_in '名前', with: 'user-name'
+          fill_in 'パスワード', with: 'user-password'
+          fill_in 'パスワード（確認用）', with: 'user-password'
+        end
+
+        context 'メールアドレスを入力しない' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            click_button '登録'
+            expect(page).to have_content 'メールアドレスを入力してください'
+            expect(page).to have_content '新規作成に失敗しました'
+            expect(current_path).to eq signup_path
+          end
+        end
+
+        context '重複したメールアドレスを入力する' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            fill_in 'メールアドレス', with: user.email
+            click_button '登録'
+            expect(page).to have_content 'メールアドレスはすでに存在します'
+            expect(page).to have_content '新規作成に失敗しました'
+            expect(current_path).to eq signup_path
+          end
+        end
+      end
+
+      describe '#password, password_confirmation' do
+        before do
+          fill_in '名前', with: 'user-name'
+          fill_in 'メールアドレス', with: 'user-email@example.com'
+        end
+
+        context 'パスワードを入力しない' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            fill_in 'パスワード（確認用）', with: 'user-password'
+            click_button '登録'
+            expect(page).to have_content 'パスワードは3文字以上で入力してください'
+            expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
+            expect(page).to have_content '新規作成に失敗しました'
+            expect(current_path).to eq signup_path
+          end
+        end
+
+        context 'パスワード（確認用）を入力しない' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            fill_in 'パスワード', with: 'user-password'
+            click_button '登録'
+            expect(page).to have_content 'パスワード（確認用）とパスワードの入力が一致しません'
+            expect(page).to have_content '新規作成に失敗しました'
+            expect(current_path).to eq signup_path
+          end
+        end
+
+        context 'パスワードを2文字入力する' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            fill_in 'パスワード', with: 'a' * 2
+            fill_in 'パスワード（確認用）', with: 'a' * 2
+            click_button '登録'
+            expect(page).to have_content 'パスワードは3文字以上で入力してください'
+            expect(page).to have_content '新規作成に失敗しました'
+            expect(current_path).to eq signup_path
+          end
+        end
+
+        context 'パスワードを3文字入力する' do
+          it 'ユーザーの新規作成が失敗し、ページが遷移しない' do
+            fill_in 'パスワード', with: 'a' * 3
+            fill_in 'パスワード（確認用）', with: 'a' * 3
+            click_button '登録'
+            expect(page).to have_content '新規作成に成功しました'
+            expect(current_path).to eq profile_path
+          end
         end
       end
     end
