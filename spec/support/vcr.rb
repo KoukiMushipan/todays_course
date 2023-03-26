32d1bea7f4b11bdac402cb873168de53e6eb5ca2
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-26 21:17:41 +0900

    Add: VCRを使わないテストでローカルホスト以外の通信を禁止に

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,13 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/support/cassettes'
   config.hook_into :webmock
-  config.allow_http_connections_when_no_cassette = true
+
+  # VCRを使っていないときの通信を許可（開発時、必要に応じて一時的に許可する）
+  # config.allow_http_connections_when_no_cassette = true
+
+  # VCRを使用していなくてもローカルホストの通信は許可する
+  config.ignore_localhost = true
+
   config.configure_rspec_metadata!
 
   # 新しいレコードの作成を許可（開発時、必要に応じて一時的にこちらに切り替える）
