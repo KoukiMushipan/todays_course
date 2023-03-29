7bcf4d95756acecfe360bb0b07d561fe1a816175
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-29 22:59:37 +0900

    Add: Departure#newで正確な住所が取得できない場合は返却するよう

--- app/forms/departure_form.rb
+++ app/forms/departure_form.rb
@@ -9,4 +9,12 @@ class DepartureForm
   validates :name, presence: true, length: { maximum: 50 }
   validates :address, presence: true, length: { maximum: 255 }
   validates :is_saved, inclusion: { in: [true, false] }
+  validate :address_format_check
+
+  private
+
+  def address_format_check
+    return unless address.present?
+    errors.add(:address, 'は〇丁目〇〇や、〇-〇〇といった形で正確に記入してください') unless address.match(/.+[\d１-９]{1,4}[-ー丁−]{1}.{0,2}[\d０-９].*/)
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -9,9 +9,17 @@ class Location < ApplicationRecord
   validates :longitude, presence: true, numericality: { in: -180..180 }
   validates :address, presence: true, length: { maximum: 255 }
   validates :place_id, presence: true, length: { maximum: 255 }
+  validate :address_format_check
 
   def self.create_from_info(location_info)
     attribute_symboles = location_info.slice(:name, :latitude, :longitude, :address, :place_id)
     create!(attribute_symboles)
+  end
+
+  private
+
+  def address_format_check
+    return unless address.present?
+    errors.add(:address, 'は〇丁目〇〇や、〇-〇〇といった形で正確に記入してください') unless address.match(/.+[\d１-９]{1,4}[-ー丁−]{1}.{0,2}[\d０-９].*/)
   end
 end
