2c55bd8a6f76f4a272cd58967a320b749bf4b78c
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-03-31 23:15:49 +0900

    Add: FactoryBot -> ランダムな地点を設定できるように

--- spec/factories/departures.rb
+++ spec/factories/departures.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :departure do
     association :user
     association :location
+
+    trait :random do
+      association :location, :random
+    end
   end
 end
--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     association :user
     association :location
     association :departure
+
+    trait :random do
+      distance { rand(1000..5000) }
+      association :departure, :random
+      association :location, :random
+    end
   end
 end
