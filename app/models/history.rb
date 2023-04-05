50723b4a046ac21c32ffa827cec81146f4bd4354
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-04-05 23:57:44 +0900

    Fix: テスト作成中に見つけた言葉遣いのミスの修正 validates on:をhistoryにも導入（ModelSpecは後で修正）

--- app/models/history.rb
+++ app/models/history.rb
@@ -5,6 +5,8 @@ class History < ApplicationRecord
 
   validates :comment, length: { maximum: 255 }
   validates :moving_distance, presence: true, numericality: { only_integer: true, in: 1..42_195 }
+  validates :start_time, presence: true, on: :update
+  validates :end_time, presence: true, on: :update
   validate :end_time_check
 
   before_create -> { self.uuid = SecureRandom.uuid }
@@ -50,8 +52,7 @@ class History < ApplicationRecord
   private
 
   def end_time_check
-    return errors.add(:start_time, 'を入力してください') if start_time.nil? && end_time
-
+    return unless start_time
     errors.add(:end_time, 'は開始時刻より遅い時間にしてください') if end_time && (start_time > end_time)
   end
 end
