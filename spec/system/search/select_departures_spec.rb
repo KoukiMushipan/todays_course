7ecce2c762d60caa90783a9ebac69deb8ff2ea4c
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-04-11 22:14:46 +0900

    Add: SystemSpec -> 出発地選択#History

--- spec/system/search/select_departures_spec.rb
+++ spec/system/search/select_departures_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Search::SelectDepartures", type: :system do
-  let(:departure) { create(:departure, is_saved: true) }
-  let(:history) { create(:history) }
+  let(:departure) { create(:departure, is_saved: true, user:) }
+  let(:destination) { create(:destination, departure:, user:) }
+  let(:history) { create(:history, destination:, user:) }
   let(:user) { create(:user) }
   let(:other) { create(:user) }
 
@@ -83,6 +84,65 @@ RSpec.describe "Search::SelectDepartures", type: :system do
 
     describe 'Start' do
       before { visit_select_saved_departures_page(departure) }
+
+      context '出発ボタンをクリックする' do
+        it '目的地の条件入力ページに遷移する' do
+          click_link '出発'
+          expect(current_url).to be_include new_search_path(departure: departure.uuid)
+        end
+      end
+    end
+  end
+
+  describe 'History' do
+    describe 'Contents' do
+      context '１つの出発地を保存する' do
+        before { visit_select_history_departure_page(history.departure) }
+
+        it '情報が正しく表示されている' do
+          expect(current_path).to eq new_departure_path
+          expect(page).to have_content history.departure.name
+          expect(page).to have_content history.departure.address
+          expect(page).to have_content I18n.l(history.start_time, format: :short)
+          expect(page).to have_content "#{history.decorate.moving_time}分"
+          expect(page).to have_content "#{history.moving_distance}m"
+          expect(page).to have_content history.destination.name
+        end
+
+        it 'リンク関連が正しく表示されている' do
+          expect(page).to have_link '出発', href: new_search_path(departure: history.departure.uuid)
+        end
+      end
+
+      context '複数のユーザーの履歴を作成する' do
+        it '自分の履歴のみ表示される' do
+          own_history = history
+          other_history = create(:history, user: other)
+          visit_select_history_departure_page(own_history)
+          expect(page).to have_content own_history.departure.name
+          expect(page).to have_content own_history.destination.name
+          expect(page).not_to have_content other_history.departure.name
+          expect(page).not_to have_content other_history.destination.name
+        end
+      end
+
+      context '保存済み・未保存出発地を作成する' do
+        it 'どちらも表示される' do
+          saved_departure_history = history
+          not_saved_departure = create(:departure, is_saved: false, user:)
+          not_saved_destination = create(:destination, user:, departure: not_saved_departure)
+          not_saved_departure_history = create(:history, user:, destination: not_saved_destination)
+          visit_select_history_departure_page(saved_departure_history.departure)
+          expect(page).to have_content saved_departure_history.departure.name
+          expect(page).to have_content saved_departure_history.destination.name
+          expect(page).to have_content not_saved_departure_history.departure.name
+          expect(page).to have_content not_saved_departure_history.destination.name
+        end
+      end
+    end
+
+    describe 'Start' do
+      before { visit_select_history_departure_page(history.departure) }
 
       context '出発ボタンをクリックする' do
         it '目的地の条件入力ページに遷移する' do
