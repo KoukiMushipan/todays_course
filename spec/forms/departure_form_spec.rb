14d7e4a9300ae10459d5e6fc1110286ca5d0c6a7
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-04-17 18:36:18 +0900

    Fix: rubocop -> spec/forms rspec-rubocopの設定追加

--- spec/forms/departure_form_spec.rb
+++ spec/forms/departure_form_spec.rb
@@ -2,98 +2,126 @@ require 'rails_helper'
 
 RSpec.describe DepartureForm, type: :model do
   describe 'Validations' do
-    it '全てのカラムを入力する' do
-      departure_form = build(:departure_form)
-      expect(departure_form.valid?(:check_save)).to be_truthy
-      expect(departure_form.errors).to be_empty
-    end
-
-    context '#name' do
-      it 'nilを渡す' do
-        departure_form = build(:departure_form, name: nil)
-        expect(departure_form).to be_invalid
-        expect(departure_form.errors[:name]).to eq ['を入力してください']
-      end
-
-      it '空の文字列を入力する' do
-        departure_form = build(:departure_form, name: '')
-        expect(departure_form).to be_invalid
-        expect(departure_form.errors[:name]).to eq ['を入力してください']
-      end
-
-      it '50文字入力する' do
-        departure_form = build(:departure_form, name: 'a' * 50)
-        expect(departure_form).to be_valid
+    context '全てのカラムを入力する' do
+      it '保存が可能である' do
+        departure_form = build(:departure_form)
+        expect(departure_form).to be_valid(:check_save)
         expect(departure_form.errors).to be_empty
-      end
-
-      it '51文字入力する' do
-        departure_form = build(:departure_form, name: 'a' * 51)
-        expect(departure_form).to be_invalid
-        expect(departure_form.errors[:name]).to eq ['は50文字以内で入力してください']
       end
     end
 
-    context '#address' do
-      it 'nilを渡す' do
-        departure_form = build(:departure_form, address: nil)
-        expect(departure_form).to be_invalid
-        expect(departure_form.errors[:address]).to eq ['を入力してください']
+    describe '#name' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, name: nil)
+          expect(departure_form).to be_invalid
+          expect(departure_form.errors[:name]).to eq ['を入力してください']
+        end
       end
 
-      it '空の文字列を入力する' do
-        departure_form = build(:departure_form, address: '')
-        expect(departure_form).to be_invalid
-        expect(departure_form.errors[:address]).to eq ['を入力してください']
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, name: '')
+          expect(departure_form).to be_invalid
+          expect(departure_form.errors[:name]).to eq ['を入力してください']
+        end
       end
 
-      it '255文字入力する' do
-        departure_form = build(:departure_form, address: 'a' * 255)
-        expect(departure_form).to be_valid
-        expect(departure_form.errors).to be_empty
+      context '50文字入力する' do
+        it '保存が可能である' do
+          departure_form = build(:departure_form, name: 'a' * 50)
+          expect(departure_form).to be_valid
+          expect(departure_form.errors).to be_empty
+        end
       end
 
-      it '256文字入力する' do
-        departure_form = build(:departure_form, address: 'a' * 256)
-        expect(departure_form).to be_invalid
-        expect(departure_form.errors[:address]).to eq ['は255文字以内で入力してください']
+      context '51文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, name: 'a' * 51)
+          expect(departure_form).to be_invalid
+          expect(departure_form.errors[:name]).to eq ['は50文字以内で入力してください']
+        end
       end
     end
 
-    context '#is_saved' do
-      it 'nilを渡す' do
-        departure_form = build(:departure_form, is_saved: nil)
-        expect(departure_form.is_saved).to be_nil
-        expect(departure_form.valid?(:check_save)).to be_falsey
-        expect(departure_form.errors[:is_saved]).to eq ['は一覧にありません']
+    describe '#address' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, address: nil)
+          expect(departure_form).to be_invalid
+          expect(departure_form.errors[:address]).to eq ['を入力してください']
+        end
       end
 
-      it '空の文字列を入力する' do
-        departure_form = build(:departure_form, is_saved: '')
-        expect(departure_form.is_saved).to be_nil
-        expect(departure_form.valid?(:check_save)).to be_falsey
-        expect(departure_form.errors[:is_saved]).to eq ['は一覧にありません']
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, address: '')
+          expect(departure_form).to be_invalid
+          expect(departure_form.errors[:address]).to eq ['を入力してください']
+        end
       end
 
-      it '空ではない文字列を入力する' do
-        departure_form = build(:departure_form, is_saved: ' ')
-        expect(departure_form.is_saved).to be_truthy
-        expect(departure_form.valid?(:check_save)).to be_truthy
-        expect(departure_form.errors).to be_empty
+      context '255文字入力する' do
+        it '保存が可能である' do
+          departure_form = build(:departure_form, address: 'a' * 255)
+          expect(departure_form).to be_valid
+          expect(departure_form.errors).to be_empty
+        end
       end
 
-      it '0を入力する' do
-        departure_form = build(:departure_form, is_saved: 0)
-        expect(departure_form.is_saved).to be_falsey
-        expect(departure_form.valid?(:check_save)).to be_truthy
-        expect(departure_form.errors).to be_empty
+      context '256文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, address: 'a' * 256)
+          expect(departure_form).to be_invalid
+          expect(departure_form.errors[:address]).to eq ['は255文字以内で入力してください']
+        end
+      end
+    end
+
+    describe '#is_saved' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, is_saved: nil)
+          expect(departure_form.is_saved).to be_nil
+          expect(departure_form).to be_invalid(:check_save)
+          expect(departure_form.errors[:is_saved]).to eq ['は一覧にありません']
+        end
       end
 
-      it '1を入力する' do
-        departure_form = build(:departure_form, is_saved: 1)
-        expect(departure_form.is_saved).to be_truthy
-        expect(departure_form.valid?(:check_save)).to be_truthy
-        expect(departure_form.errors).to be_empty
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          departure_form = build(:departure_form, is_saved: '')
+          expect(departure_form.is_saved).to be_nil
+          expect(departure_form).to be_invalid(:check_save)
+          expect(departure_form.errors[:is_saved]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '空ではない文字列を入力する' do
+        it '保存が可能である' do
+          departure_form = build(:departure_form, is_saved: ' ')
+          expect(departure_form.is_saved).to be_truthy
+          expect(departure_form).to be_valid(:check_save)
+          expect(departure_form.errors).to be_empty
+        end
+      end
+
+      context '0を入力する' do
+        it '保存が可能である' do
+          departure_form = build(:departure_form, is_saved: 0)
+          expect(departure_form.is_saved).to be_falsey
+          expect(departure_form).to be_valid(:check_save)
+          expect(departure_form.errors).to be_empty
+        end
+      end
+
+      context '1を入力する' do
+        it '保存が可能である' do
+          departure_form = build(:departure_form, is_saved: 1)
+          expect(departure_form.is_saved).to be_truthy
+          expect(departure_form).to be_valid(:check_save)
+          expect(departure_form.errors).to be_empty
+        end
       end
     end
   end
--- spec/forms/search_term_form_spec.rb
+++ spec/forms/search_term_form_spec.rb
@@ -2,73 +2,103 @@ require 'rails_helper'
 
 RSpec.describe SearchTermForm, type: :model do
   describe 'Validations' do
-    context '#radius' do
-      it 'nilを渡す' do
-        search_term_form = build(:search_term_form, radius: nil)
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:radius]).to eq ['を入力してください', 'は数値で入力してください']
-      end
-
-      it '空の文字列を入力する' do
-        search_term_form = build(:search_term_form, radius: '')
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:radius]).to eq ['を入力してください', 'は数値で入力してください']
-      end
-
-      it '999を入力する' do
-        search_term_form = build(:search_term_form, radius: 999)
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:radius]).to eq ['は1000m~5,000m以内に設定してください']
-      end
-
-      it '1_000を入力する' do
-        search_term_form = build(:search_term_form, radius: 1_000)
+    context '全てのカラムを入力する' do
+      it '保存が可能である' do
+        search_term_form = build(:search_term_form)
         expect(search_term_form).to be_valid
-        expect(search_term_form.errors[:radius]).to be_empty
-      end
-
-      it '5_000を入力する' do
-        search_term_form = build(:search_term_form, radius: 5000)
-        expect(search_term_form).to be_valid
-        expect(search_term_form.errors[:radius]).to be_empty
-      end
-
-      it '5_001を入力する' do
-        search_term_form = build(:search_term_form, radius: 5_001)
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:radius]).to eq ['は1000m~5,000m以内に設定してください']
+        expect(search_term_form.errors).to be_empty
       end
     end
 
-    context '#type' do
-      it 'nilを渡す' do
-        search_term_form = build(:search_term_form, type: nil)
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:type]).to eq ['を入力してください', 'は一覧にありません']
+    describe '#radius' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, radius: nil)
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:radius]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '空の文字列を入力する' do
-        search_term_form = build(:search_term_form, type: '')
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:type]).to eq ['を入力してください', 'は一覧にありません']
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, radius: '')
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:radius]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '一覧にない文字列を入力する' do
-        search_term_form = build(:search_term_form, type: 'a')
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:type]).to eq ['は一覧にありません']
+      context '999を入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, radius: 999)
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:radius]).to eq ['は1000m~5,000m以内に設定してください']
+        end
       end
 
-      it '255文字入力する' do
-        search_term_form = build(:search_term_form, type: 'a' * 255)
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:type]).to eq ['は一覧にありません']
+      context '1_000を入力する' do
+        it '保存が可能である' do
+          search_term_form = build(:search_term_form, radius: 1_000)
+          expect(search_term_form).to be_valid
+          expect(search_term_form.errors[:radius]).to be_empty
+        end
       end
 
-      it '256文字入力する' do
-        search_term_form = build(:search_term_form, type: 'a' * 256)
-        expect(search_term_form).to be_invalid
-        expect(search_term_form.errors[:type]).to eq ['は一覧にありません', 'は255文字以内で入力してください']
+      context '5_000を入力する' do
+        it '保存が可能である' do
+          search_term_form = build(:search_term_form, radius: 5000)
+          expect(search_term_form).to be_valid
+          expect(search_term_form.errors[:radius]).to be_empty
+        end
+      end
+
+      context '5_001を入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, radius: 5_001)
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:radius]).to eq ['は1000m~5,000m以内に設定してください']
+        end
+      end
+    end
+
+    describe '#type' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, type: nil)
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:type]).to eq %w[を入力してください は一覧にありません]
+        end
+      end
+
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, type: '')
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:type]).to eq %w[を入力してください は一覧にありません]
+        end
+      end
+
+      context '一覧にない文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, type: 'a')
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:type]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '255文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, type: 'a' * 255)
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:type]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '256文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          search_term_form = build(:search_term_form, type: 'a' * 256)
+          expect(search_term_form).to be_invalid
+          expect(search_term_form.errors[:type]).to eq %w[は一覧にありません は255文字以内で入力してください]
+        end
       end
     end
   end
