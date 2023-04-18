d4c8a23106501e16d592e36179394466afa907f8
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-04-18 14:59:39 +0900

    Fix: rubocop -> spec/models

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,199 +1,255 @@
 require 'rails_helper'
 
-RSpec.describe Destination, type: :model do
-describe 'Validations' do
-    it '全てのカラムを入力する' do
-      destination = build(:destination)
-      expect(destination).to be_valid
-      expect(destination.errors).to be_empty
-    end
-
-    context '#comment' do
-      it 'nilを渡す' do
-        destination = build(:destination, comment: nil)
-        expect(destination).to be_valid
-        expect(destination.errors[:comment]).to be_empty
-      end
-
-      it '空の文字列を入力する' do
-        destination = build(:destination, comment: '')
-        expect(destination).to be_valid
-        expect(destination.errors[:comment]).to be_empty
-      end
-
-      it '255文字入力する' do
-        destination = build(:destination, comment: 'a' * 255)
-        expect(destination).to be_valid
-        expect(destination.errors).to be_empty
-      end
-
-      it '256文字入力する' do
-        destination = build(:destination, comment: 'a' * 256)
-        expect(destination).to be_invalid
-        expect(destination.errors[:comment]).to eq ['は255文字以内で入力してください']
-      end
-    end
-
-    context '#distance' do
-      it 'nilを渡す' do
-        destination = build(:destination, distance: nil)
-        expect(destination).to be_invalid
-        expect(destination.errors[:distance]).to eq ['を入力してください', 'は数値で入力してください']
-      end
-
-      it '空の文字列を入力する' do
-        destination = build(:destination, distance: '')
-        expect(destination).to be_invalid
-        expect(destination.errors[:distance]).to eq ['を入力してください', 'は数値で入力してください']
-      end
-
-      it '0を入力する' do
-        destination = build(:destination, distance: 0)
-        expect(destination).to be_invalid
-        expect(destination.errors[:distance]).to eq ['は1m~21,097m以内に設定してください']
-      end
-
-      it '１を入力する' do
-        destination = build(:destination, distance: 1)
-        expect(destination).to be_valid
-        expect(destination.errors[:distance]).to be_empty
-      end
-
-      it '21_097を入力する' do
-        destination = build(:destination, distance: 21_097)
-        expect(destination).to be_valid
-        expect(destination.errors[:distance]).to be_empty
-      end
-
-      it '21_098を入力する' do
-        destination = build(:destination, distance: 21_098)
-        expect(destination).to be_invalid
-        expect(destination.errors[:distance]).to eq ['は1m~21,097m以内に設定してください']
-      end
-    end
-
-    context '#is_saved' do
-      it 'nilを渡す' do
-        destination = build(:destination, is_saved: nil)
-        expect(destination.is_saved).to be_nil
-        expect(destination).to be_invalid
-        expect(destination.errors[:is_saved]).to eq ['は一覧にありません']
-      end
-
-      it '空の文字列を入力する' do
-        destination = build(:destination, is_saved: '')
-        expect(destination.is_saved).to be_nil
-        expect(destination).to be_invalid
-        expect(destination.errors[:is_saved]).to eq ['は一覧にありません']
-      end
-
-      it '空ではない文字列を入力する' do
-        destination = build(:destination, is_saved: ' ')
-        expect(destination.is_saved).to be_truthy
-        expect(destination).to be_valid
-        expect(destination.errors).to be_empty
-      end
-
-      it '0を入力する' do
-        destination = build(:destination, is_saved: 0)
-        expect(destination.is_saved).to be_falsey
-        expect(destination).to be_valid
-        expect(destination.errors).to be_empty
-      end
-
-      it '1を入力する' do
-        destination = build(:destination, is_saved: 1)
-        expect(destination.is_saved).to be_truthy
+RSpec.describe Destination do
+  describe 'Validations' do
+    context '全てのカラムを入力する' do
+      it '保存が可能である' do
+        destination = build(:destination)
         expect(destination).to be_valid
         expect(destination.errors).to be_empty
       end
     end
 
-    context '#is_published_comment' do
-      it 'nilを渡す' do
-        destination = build(:destination, is_published_comment: nil)
-        expect(destination).to be_invalid
-        expect(destination.errors[:is_published_comment]).to eq ['は一覧にありません']
+    describe '#comment' do
+      context 'nilを渡す' do
+        it '保存が可能である' do
+          destination = build(:destination, comment: nil)
+          expect(destination).to be_valid
+          expect(destination.errors[:comment]).to be_empty
+        end
       end
 
-      it '空の文字列を入力する' do
-        destination = build(:destination, is_published_comment: '')
-        expect(destination.is_published_comment).to be_nil
-        expect(destination).to be_invalid
-        expect(destination.errors[:is_published_comment]).to eq ['は一覧にありません']
+      context '空の文字列を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, comment: '')
+          expect(destination).to be_valid
+          expect(destination.errors[:comment]).to be_empty
+        end
       end
 
-      it '空ではない文字列を入力する' do
-        destination = build(:destination, is_published_comment: ' ')
-        expect(destination.is_published_comment).to be_truthy
-        expect(destination).to be_valid
-        expect(destination.errors).to be_empty
+      context '255文字入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, comment: 'a' * 255)
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
       end
 
-      it '0を入力する' do
-        destination = build(:destination, is_published_comment: 0)
-        expect(destination.is_published_comment).to be_falsey
-        expect(destination).to be_valid
-        expect(destination.errors).to be_empty
-      end
-
-      it '1を入力する' do
-        destination = build(:destination, is_published_comment: 1)
-        expect(destination.is_published_comment).to be_truthy
-        expect(destination).to be_valid
-        expect(destination.errors).to be_empty
+      context '256文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, comment: 'a' * 256)
+          expect(destination).to be_invalid
+          expect(destination.errors[:comment]).to eq ['は255文字以内で入力してください']
+        end
       end
     end
 
-    context '#uuid' do
-      it 'nilを渡す' do
-        destination = build(:destination, uuid: nil)
-        expect(destination).to be_valid
-        expect(destination.errors[:uuid]).to be_empty
+    describe '#distance' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, distance: nil)
+          expect(destination).to be_invalid
+          expect(destination.errors[:distance]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '空の文字列を入力する' do
-        destination = build(:destination, uuid: '')
-        expect(destination).to be_valid
-        expect(destination.errors[:uuid]).to be_empty
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, distance: '')
+          expect(destination).to be_invalid
+          expect(destination.errors[:distance]).to eq %w[を入力してください は数値で入力してください]
+        end
+      end
+
+      context '0を入力する' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, distance: 0)
+          expect(destination).to be_invalid
+          expect(destination.errors[:distance]).to eq ['は1m~21,097m以内に設定してください']
+        end
+      end
+
+      context '１を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, distance: 1)
+          expect(destination).to be_valid
+          expect(destination.errors[:distance]).to be_empty
+        end
+      end
+
+      context '21_097を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, distance: 21_097)
+          expect(destination).to be_valid
+          expect(destination.errors[:distance]).to be_empty
+        end
+      end
+
+      context '21_098を入力する' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, distance: 21_098)
+          expect(destination).to be_invalid
+          expect(destination.errors[:distance]).to eq ['は1m~21,097m以内に設定してください']
+        end
+      end
+    end
+
+    describe '#is_saved' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, is_saved: nil)
+          expect(destination.is_saved).to be_nil
+          expect(destination).to be_invalid
+          expect(destination.errors[:is_saved]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, is_saved: '')
+          expect(destination.is_saved).to be_nil
+          expect(destination).to be_invalid
+          expect(destination.errors[:is_saved]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '空ではない文字列を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, is_saved: ' ')
+          expect(destination.is_saved).to be_truthy
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
+      end
+
+      context '0を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, is_saved: 0)
+          expect(destination.is_saved).to be_falsey
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
+      end
+
+      context '1を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, is_saved: 1)
+          expect(destination.is_saved).to be_truthy
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
+      end
+    end
+
+    describe '#is_published_comment' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, is_published_comment: nil)
+          expect(destination).to be_invalid
+          expect(destination.errors[:is_published_comment]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, is_published_comment: '')
+          expect(destination.is_published_comment).to be_nil
+          expect(destination).to be_invalid
+          expect(destination.errors[:is_published_comment]).to eq ['は一覧にありません']
+        end
+      end
+
+      context '空ではない文字列を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, is_published_comment: ' ')
+          expect(destination.is_published_comment).to be_truthy
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
+      end
+
+      context '0を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, is_published_comment: 0)
+          expect(destination.is_published_comment).to be_falsey
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
+      end
+
+      context '1を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, is_published_comment: 1)
+          expect(destination.is_published_comment).to be_truthy
+          expect(destination).to be_valid
+          expect(destination.errors).to be_empty
+        end
+      end
+    end
+
+    describe '#uuid' do
+      context 'nilを渡す' do
+        it '保存が可能である' do
+          destination = build(:destination, uuid: nil)
+          expect(destination).to be_valid
+          expect(destination.errors[:uuid]).to be_empty
+        end
+      end
+
+      context '空の文字列を入力する' do
+        it '保存が可能である' do
+          destination = build(:destination, uuid: '')
+          expect(destination).to be_valid
+          expect(destination.errors[:uuid]).to be_empty
+        end
       end
     end
   end
 
   describe 'Associations' do
-    context '#user' do
-      it 'nilを渡す' do
-        destination = build(:destination, user: nil)
-        expect(destination).to be_invalid
-        expect(destination.errors[:user]).to eq ['を入力してください']
+    describe '#user' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, user: nil)
+          expect(destination).to be_invalid
+          expect(destination.errors[:user]).to eq ['を入力してください']
+        end
       end
     end
 
-    context '#location' do
-      it 'nilを渡す' do
-        destination = build(:destination, location: nil)
-        expect(destination).to be_invalid
-        expect(destination.errors[:location]).to eq ['を入力してください']
+    describe '#location' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, location: nil)
+          expect(destination).to be_invalid
+          expect(destination.errors[:location]).to eq ['を入力してください']
+        end
       end
 
-      it '作成したdestinationを削除すると関連付けされたlocationも削除される' do
-        destination = create(:destination)
-        expect { destination.destroy }.to change { Location.count }.from(2).to(1)
-      end
-    end
-
-    context '#departure' do
-      it 'nilを渡す' do
-        destination = build(:destination, departure: nil)
-        expect(destination).to be_invalid
-        expect(destination.errors[:departure]).to eq ['を入力してください']
+      context '作成したdestinationを削除する' do
+        it '関連付けされたlocationも削除される' do
+          destination = create(:destination)
+          expect { destination.destroy }.to change(Location, :count).from(2).to(1)
+        end
       end
     end
 
-    context '#history' do
-      it '作成したdestinationを削除すると関連付けされたhistoryも削除される' do
-        history = create(:history)
-        expect { history.destination.destroy }.to change { History.count }.from(1).to(0)
+    describe '#departure' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          destination = build(:destination, departure: nil)
+          expect(destination).to be_invalid
+          expect(destination.errors[:departure]).to eq ['を入力してください']
+        end
+      end
+    end
+
+    describe '#history' do
+      context '作成したdestinationを削除する' do
+        it '関連付けされたhistoryも削除される' do
+          history = create(:history)
+          expect { history.destination.destroy }.to change(History, :count).from(1).to(0)
+        end
       end
     end
   end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,176 +1,230 @@
 require 'rails_helper'
 
-RSpec.describe Location, type: :model do
+RSpec.describe Location do
   describe 'Validations' do
-    it '全てのカラムを入力する' do
-      location = build(:location, :for_departure)
-      expect(location).to be_valid
-      expect(location.errors).to be_empty
-    end
-
-    context '#name' do
-      it 'nilを渡す' do
-        location = build(:location, :for_departure, name: nil)
-        expect(location).to be_invalid
-        expect(location.errors[:name]).to eq ['を入力してください']
-      end
-
-      it '空の文字列を入力する' do
-        location = build(:location, :for_departure, name: '')
-        expect(location).to be_invalid
-        expect(location.errors[:name]).to eq ['を入力してください']
-      end
-
-      it '50文字入力する' do
-        location = build(:location, :for_departure, name: 'a' * 50)
+    context '全てのカラムを入力する' do
+      it '保存が可能である' do
+        location = build(:location, :for_departure)
         expect(location).to be_valid
         expect(location.errors).to be_empty
-      end
-
-      it '51文字入力する' do
-        location = build(:location, :for_departure, name: 'a' * 51)
-        expect(location).to be_invalid
-        expect(location.errors[:name]).to eq ['は50文字以内で入力してください']
       end
     end
 
-    context '#latitude' do
-      it 'nilを渡す' do
-        location = build(:location, :for_departure, latitude: nil)
-        expect(location).to be_invalid
-        expect(location.errors[:latitude]).to eq ['を入力してください', 'は数値で入力してください']
+    describe '#name' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, name: nil)
+          expect(location).to be_invalid
+          expect(location.errors[:name]).to eq ['を入力してください']
+        end
       end
 
-      it '空の文字列を入力する' do
-        location = build(:location, :for_departure, latitude: '')
-        expect(location).to be_invalid
-        expect(location.errors[:latitude]).to eq ['を入力してください', 'は数値で入力してください']
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, name: '')
+          expect(location).to be_invalid
+          expect(location.errors[:name]).to eq ['を入力してください']
+        end
       end
 
-      it '整数を入力する' do
-        location = build(:location, :for_departure, latitude: 1)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
+      context '50文字入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, name: 'a' * 50)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
       end
 
-      it '-91を入力する' do
-        location = build(:location, :for_departure, latitude: -91)
-        expect(location).to be_invalid
-        expect(location.errors[:latitude]).to eq ['は-90度~90度以内に設定してください']
-      end
-
-      it '-90を入力する' do
-        location = build(:location, :for_departure, latitude: -90)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
-      end
-
-      it '90を入力する' do
-        location = build(:location, :for_departure, latitude: 90)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
-      end
-
-      it '91を入力する' do
-        location = build(:location, :for_departure, latitude: 91)
-        expect(location).to be_invalid
-        expect(location.errors[:latitude]).to eq ['は-90度~90度以内に設定してください']
+      context '51文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, name: 'a' * 51)
+          expect(location).to be_invalid
+          expect(location.errors[:name]).to eq ['は50文字以内で入力してください']
+        end
       end
     end
 
-    context '#longitude' do
-      it 'nilを渡す' do
-        location = build(:location, :for_departure, longitude: nil)
-        expect(location).to be_invalid
-        expect(location.errors[:longitude]).to eq ['を入力してください', 'は数値で入力してください']
+    describe '#latitude' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, latitude: nil)
+          expect(location).to be_invalid
+          expect(location.errors[:latitude]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '空の文字列を入力する' do
-        location = build(:location, :for_departure, longitude: '')
-        expect(location).to be_invalid
-        expect(location.errors[:longitude]).to eq ['を入力してください', 'は数値で入力してください']
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, latitude: '')
+          expect(location).to be_invalid
+          expect(location.errors[:latitude]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '整数を入力する' do
-        location = build(:location, :for_departure, longitude: 1)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
+      context '整数を入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, latitude: 1)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
       end
 
-      it '-181を入力する' do
-        location = build(:location, :for_departure, longitude: -181)
-        expect(location).to be_invalid
-        expect(location.errors[:longitude]).to eq ['は-180度~180度以内に設定してください']
+      context '-91を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, latitude: -91)
+          expect(location).to be_invalid
+          expect(location.errors[:latitude]).to eq ['は-90度~90度以内に設定してください']
+        end
       end
 
-      it '-180を入力する' do
-        location = build(:location, :for_departure, longitude: -180)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
+      context '-90を入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, latitude: -90)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
       end
 
-      it '180を入力する' do
-        location = build(:location, :for_departure, longitude: 180)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
+      context '90を入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, latitude: 90)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
       end
 
-      it '181を入力する' do
-        location = build(:location, :for_departure, longitude: 181)
-        expect(location).to be_invalid
-        expect(location.errors[:longitude]).to eq ['は-180度~180度以内に設定してください']
-      end
-    end
-
-    context '#address' do
-      it 'nilを渡す' do
-        location = build(:location, :for_departure, address: nil)
-        expect(location).to be_invalid
-        expect(location.errors[:address]).to eq ['を入力してください']
-      end
-
-      it '空の文字列を入力する' do
-        location = build(:location, :for_departure, address: '')
-        expect(location).to be_invalid
-        expect(location.errors[:address]).to eq ['を入力してください']
-      end
-
-      it '255文字入力する' do
-        location = build(:location, :for_departure, address: 'a' * 255)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
-      end
-
-      it '256文字入力する' do
-        location = build(:location, :for_departure, address: 'a' * 256)
-        expect(location).to be_invalid
-        expect(location.errors[:address]).to eq ['は255文字以内で入力してください']
+      context '91を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, latitude: 91)
+          expect(location).to be_invalid
+          expect(location.errors[:latitude]).to eq ['は-90度~90度以内に設定してください']
+        end
       end
     end
 
-    context '#place_id' do
-      it 'nilを渡す' do
-        location = build(:location, :for_departure, place_id: nil)
-        expect(location).to be_invalid
-        expect(location.errors[:place_id]).to eq ['を入力してください']
+    describe '#longitude' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, longitude: nil)
+          expect(location).to be_invalid
+          expect(location.errors[:longitude]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '空の文字列を入力する' do
-        location = build(:location, :for_departure, place_id: '')
-        expect(location).to be_invalid
-        expect(location.errors[:place_id]).to eq ['を入力してください']
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, longitude: '')
+          expect(location).to be_invalid
+          expect(location.errors[:longitude]).to eq %w[を入力してください は数値で入力してください]
+        end
       end
 
-      it '255文字入力する' do
-        location = build(:location, :for_departure, place_id: 'a' * 255)
-        expect(location).to be_valid
-        expect(location.errors).to be_empty
+      context '整数を入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, longitude: 1)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
       end
 
-      it '256文字入力する' do
-        location = build(:location, :for_departure, place_id: 'a' * 256)
-        expect(location).to be_invalid
-        expect(location.errors[:place_id]).to eq ['は255文字以内で入力してください']
+      context '-181を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, longitude: -181)
+          expect(location).to be_invalid
+          expect(location.errors[:longitude]).to eq ['は-180度~180度以内に設定してください']
+        end
+      end
+
+      context '-180を入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, longitude: -180)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
+      end
+
+      context '180を入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, longitude: 180)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
+      end
+
+      context '181を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, longitude: 181)
+          expect(location).to be_invalid
+          expect(location.errors[:longitude]).to eq ['は-180度~180度以内に設定してください']
+        end
+      end
+    end
+
+    describe '#address' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, address: nil)
+          expect(location).to be_invalid
+          expect(location.errors[:address]).to eq ['を入力してください']
+        end
+      end
+
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, address: '')
+          expect(location).to be_invalid
+          expect(location.errors[:address]).to eq ['を入力してください']
+        end
+      end
+
+      context '255文字入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, address: 'a' * 255)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
+      end
+
+      context '256文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, address: 'a' * 256)
+          expect(location).to be_invalid
+          expect(location.errors[:address]).to eq ['は255文字以内で入力してください']
+        end
+      end
+    end
+
+    describe '#place_id' do
+      context 'nilを渡す' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, place_id: nil)
+          expect(location).to be_invalid
+          expect(location.errors[:place_id]).to eq ['を入力してください']
+        end
+      end
+
+      context '空の文字列を入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, place_id: '')
+          expect(location).to be_invalid
+          expect(location.errors[:place_id]).to eq ['を入力してください']
+        end
+      end
+
+      context '255文字入力する' do
+        it '保存が可能である' do
+          location = build(:location, :for_departure, place_id: 'a' * 255)
+          expect(location).to be_valid
+          expect(location.errors).to be_empty
+        end
+      end
+
+      context '256文字入力する' do
+        it 'バリデーションエラーが発生する' do
+          location = build(:location, :for_departure, place_id: 'a' * 256)
+          expect(location).to be_invalid
+          expect(location.errors[:place_id]).to eq ['は255文字以内で入力してください']
+        end
       end
     end
   end
