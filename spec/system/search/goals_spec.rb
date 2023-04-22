4ae87d608183c57c3cca4ec5671b7f724ec4b970
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-04-22 14:31:04 +0900

    Add: SystemSpec -> ゴール#Goal, Cancel

--- spec/system/search/goals_spec.rb
+++ spec/system/search/goals_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Search::Goals' do
   let(:departure) { create(:departure) }
   let(:destination) { create(:destination, departure: departure, user: departure.user) }
   let(:not_finished_history) { create(:history, :not_finished, destination: destination, user: destination.user) }
-  # let(:history) { create(:history, destination: destination, user: destination.user) }
 
   describe 'Page' do
     context 'スタートし、ゴールページに遷移する' do
@@ -33,6 +32,34 @@ RSpec.describe 'Search::Goals' do
         expect(find('a', text: 'ゴール')['data-turbo-method']).to eq 'patch'
         expect(page).to have_link 'やめる', href: history_path(not_finished_history.uuid, route: 'moving_page')
         expect(find('a', text: 'やめる')['data-turbo-method']).to eq 'delete'
+      end
+    end
+  end
+
+  describe 'Goal' do
+    before { visit_goal_page_from_not_finished(not_finished_history) }
+
+    context 'ゴールする' do
+      it 'ゴールした履歴が表示される' do
+        click_link 'ゴール'
+        expect(page).to have_current_path history_path(not_finished_history.uuid)
+        expect(page).to have_content 'ゴールしました'
+        expect(page).to have_content "GOAL: #{I18n.l(History.last.end_time, format: :short)}"
+      end
+    end
+  end
+
+  describe 'Cancel' do
+    before { visit_goal_page_from_not_finished(not_finished_history) }
+
+    context 'やめるをクリックする' do
+      it '履歴が削除され、スタートページに遷移する' do
+        page.accept_confirm("スタート記録は削除されます。\nよろしいですか？") do
+          click_link 'やめる'
+        end
+        expect(page).to have_current_path new_history_path(destination: not_finished_history.destination.uuid)
+        expect(page).to have_content '取り消しました'
+        expect(page).to have_content not_finished_history.destination.name
       end
     end
   end
