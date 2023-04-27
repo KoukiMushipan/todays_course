0cd27b2a36108387e3523b004147c13565aad3ed
KoukiMushipan <mushipan.gyunyu@gmail.com>
2023-04-27 13:11:19 +0900

    Add: SystemSpec -> ゲスト向け検索結果ページ#Page, Contents

--- spec/support/visit_macros.rb
+++ spec/support/visit_macros.rb
@@ -102,4 +102,15 @@ module VisitMacros
     sleep(0.1)
     visit history_path(history.uuid)
   end
+
+  def visit_results_for_guest_page
+    for_departure = build(:location, :for_departure)
+    geocode_mock(for_departure.attributes.compact.symbolize_keys)
+    nearby_mock(Settings.nearby_result.radius_1000.to_hash)
+    visit new_guest_path
+    fill_in '出発地の住所', with: for_departure.address
+    fill_in '距離(1000m~5000m)', with: 5000
+    select 'コンビニエンスストア', from: '目的地の種類'
+    click_button '検索'
+  end
 end
